5460cb0228c7319efd9377049401628b637a1525
Slayver Sayto <slaysayto@gmail.com>
2019-03-06 17:52:53 +0100

    fix simple bug

--- routes/cars.js
+++ routes/cars.js
@@ -23,7 +23,7 @@ router.post('/', async (req, res) => {
     if (error) return res.status(400).send(error.details[0].message);
     // verify that the agency exist
     const agency = await Agency.findOne({_id: req.body.agency});
-    if (!agency) if (!car) return res.status(404).send(' The agency with the giving id was not found');
+    if (!agency) return res.status(404).send(' The agency with the giving id was not found');
     // save the new car
     const car = new Car(req.body);
     await car.save();
