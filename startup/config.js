1dc44139952161115bdcda60b9d263d6de0d980b
Slayver Sayto <slaysayto@gmail.com>
2019-03-11 16:00:19 +0100

    add patch method

--- startup/config.js
+++ startup/config.js
@@ -10,7 +10,7 @@ module.exports = (app) => {
         }
         app.use(function(req, res, next) {
             res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+            res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS, PATCH");
             res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept ,Authorization");
            next();
           });
