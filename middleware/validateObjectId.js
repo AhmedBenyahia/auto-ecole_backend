6c93356841ed21c261f8aac5c7b57d01eaea6c9c
Slayver Sayto <slaysayto@gmail.com>
2019-03-13 08:58:15 +0100

    delete unused file

--- middleware/validateObjectId.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const {logger} = require('../startup/logging');
-module.exports = (req, res, next) => {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-        logger.info(` Invalid id ${req.params.id}`);
-        return res.status(404).send('Invalid Id');
-    }
-    next();
-};
