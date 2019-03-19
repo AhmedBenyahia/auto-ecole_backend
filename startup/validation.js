6677ba286914c064be7ed3f1125acfdaab4ed9be
Slayver Sayto <slaysayto@gmail.com>
2019-03-19 08:56:48 +0100

    add special validation for cin number

--- startup/validation.js
+++ startup/validation.js
@@ -8,7 +8,8 @@ const joiExtended = Joi.extend({
         mobile: 'needs to be a valid phone number according to E.164 international format',
         email: 'needs to be a valid email format',
         objectId: 'needs to be a valid ID',
-        cin: 'needs to be a valid cin number'
+        cin: 'needs to be a valid cin number',
+        numPlate: 'need to be a valid tunisienne car serial number'
 
     },
     rules: [{
@@ -44,6 +45,15 @@ const joiExtended = Joi.extend({
             const reg = RegExp(/^[0-9]{8}$/);
             if (!reg.test(value)) {
                 return this.createError('string.cin', {v: value}, state, options);
+            }
+            return value
+        }
+    },{
+        name: 'numPlate',
+        validate(params, value, state, options) {
+            const reg = RegExp(/^([0-9]{3}TN[0-9]{1,4})|(RT[0-9]{6})$/);
+            if (!reg.test(value)) {
+                return this.createError('string.numPlate', {v: value}, state, options);
             }
             return value
         }
